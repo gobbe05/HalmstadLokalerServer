555de26e52051a4be2a75c25d788bbea6fdd7dfc
Gabriel Raskov <gabriel@raskov.se>
2024-11-29 07:23:22 +0100

    Utility funcion to increment views when fetching offices

---
+++ src/utils/incrementAndFetchOffices.ts
@@ -0,0 +1,31 @@
+import Office, { IOffice } from '../models/officeModel'
+
+/**
+ * Increment views for a batch of documents and return the updated documents.
+ * @param filter - MongoDB filter to match documents.
+ * @param limit - Limit
+ * @returns Promise<IOffice[]> - Array of updated documents.
+ */
+export const incrementAndFetchOffices = async (
+  filter: Record<string, unknown>, // Generic filter type
+  limit?: number
+): Promise<IOffice[]> => {
+  try {
+    // Find documents matching the filter with a limit
+    const matchingDocuments = limit ? await Office.find(filter).limit(limit) : await Office.find(filter)
+
+    // Extract the IDs of the limited documents
+    const ids = matchingDocuments.map((doc) => doc._id);
+
+    // Increment views for all matching documents
+    await Office.updateMany({ _id: { $in: ids } }, { $inc: { views: 1 } });
+
+    // Fetch the updated documents
+    const updatedDocuments = limit ? await Office.find(filter).limit(limit) : await Office.find(filter)  
+
+    return updatedDocuments; // Cast result to match the expected type
+  } catch (error) {
+    console.error('Error incrementing and fetching batch:', error);
+    throw new Error('Failed to increment and fetch batch');
+  }
+}
---
+++ src/utils/incrementAndFetchOneOffice.ts
@@ -0,0 +1,23 @@
+import Office, { IOffice } from '../models/officeModel'
+
+/**
+ * Increment views for a batch of documents and return the updated documents.
+ * @param filter - MongoDB filter to match documents.
+ * @returns Promise<IOffice[]> - Array of updated documents.
+ */
+export const incrementAndFetchOneOffice = async (
+  filter: Record<string, unknown>, // Generic filter type
+): Promise<IOffice> => {
+  try {
+    // Increment views for the office document
+    await Office.updateOne(filter, { $inc: { views: 1 } });
+
+    // Fetch the updated documents
+    const office = await Office.findOne(filter);  
+
+    return office as IOffice; // Cast result to match the expected type
+  } catch (error) {
+    console.error('Error incrementing and fetching batch:', error);
+    throw new Error('Failed to increment and fetch batch');
+  }
+}
