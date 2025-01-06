3ea526b37d67393e64d08d806c0739d668ef7765
Gabriel Raskov <gabriel@raskov.se>
2025-01-06 15:30:05 +0100

    Byggt om meddelanden

--- src/controllers/messageController.ts
+++ src/controllers/messageController.ts
@@ -1,36 +1,76 @@
 import { Request, Response} from 'express'
-import Conversation from '../models/conversationModel'
-import User, { IUser } from '../models/userModel'
+import { IUser } from '../models/userModel'
 import handleMongooseError from '../utils/errorHandler'
-import { MongooseError } from 'mongoose'
+import { MongooseError, Types } from 'mongoose'
 import Message from '../models/messageModel'
 
 // GET /api/message/all
 export const getAllMessages = async (req: Request, res: Response) => {
-    const conversations = await Conversation.find({})
     const messages = await Message.find({})
 
-    return res.status(200).json({status: "OK", messages, conversations})
+    return res.status(200).json({status: "OK", messages})
 }
 
-// GET /api/message/:id
-
+// GET /api/message
 export const getMessages = async (req: Request, res: Response) => {
-    const {id} = req.params
-    
+    const userid = (req.user as IUser)._id
     try {
-        // Check access to conversation
-        const userid = (req.user as IUser)._id
-        const testConversation = await Conversation.findOne({_id: id, $or: [{broker: userid}, {buyer: userid}]})
-        if(!testConversation) return res.status(403).json({status: "Forbidden", msg: "You don't have access to this conversation"});
-
-        const messages = await Message.find({conversation: id})
+        // TODO // Sort result
+        const messages = await Message.find({receiver: userid})
         return res.status(200).json({status: "OK", messages})
     } catch(e) {
         handleMongooseError(e as MongooseError, res)
     }
 }
+// GET /api/message/:id
+export const getMessage = async (req: Request, res: Response) => {
+    const {id} = req.params
+    try {
+        const userid = (req.user as IUser)._id
+        const message = await Message.findOne({_id: id})
+        if(!message) return res.status(404).json({status: "Not Found", msg: "Message couldn't be found"});
+        console.log("Message: " + message)
+        console.log("Userid: " + userid)
+        if(!new Types.ObjectId(userid).equals(message.receiver)) return res.status(401).json({status: "Unauthorized", msg: "You don't have access to this message"});
+        return res.status(200).json({status: "OK", message})
+    } catch(e) {
+        handleMongooseError(e as MongooseError, res)
+    }
+}
 
+// POST /api/message
+export const postMessage = async (req: Request, res: Response) => {
+    const {message, email, phone, company, receiver} = req.body
+    try {
+        if(!message) return res.status(400).json({status: "Bad Request", msg: "Message can't be empty"})
+        if(!email) return res.status(400).json({status: "Bad Request", msg: "Email can't be empty"})
+        if(!phone) return res.status(400).json({status: "Bad Request", msg: "Phone number can't be empty"})
+        if(!company) return res.status(400).json({status: "Bad Request", msg: "Company name can't be empty"})
+
+        const userid = (req.user as IUser)._id
+        const userType = (req.user as IUser).type
+
+        if(userType != "buyer") return res.status(401).json({status: "Unauthorized", msg: "You must be a buyer to send messages"})
+
+        // Create message
+        const newMessage = new Message({
+            sender: userid,
+            receiver,
+            message,
+            email,
+            company,
+            phone
+        })
+        await newMessage.save()
+
+        return res.status(200).json({status: "OK"})
+    } catch(e) {
+        handleMongooseError(e as MongooseError, res)
+    }
+}
+
+/* This type of message logic is not used/needed anymore */
+/*
 // GET /api/message/latest/:id
 export const getLatestMessage = async (req: Request, res: Response) => {
     const {id} = req.params
@@ -47,32 +87,6 @@ export const getLatestMessage = async (req: Request, res: Response) => {
         handleMongooseError(e as MongooseError, res)
     }
 }
-
-// POST /api/message
-export const postMessage = async (req: Request, res: Response) => {
-    const {message, conversation} = req.body
-    try {
-        if(!message) return res.status(400).json({status: "Bad Request", msg: "Message can't be empty"})
-
-        // Check access to conversation
-        const userid = (req.user as IUser)._id
-        const testConversation = await Conversation.findOne({_id: conversation, $or: [{broker: userid}, {buyer: userid}]})
-        if(!testConversation) return res.status(403).json({status: "Forbidden", msg: "You don't have access to this conversation"});
-
-        // Create message
-        const newMessage = new Message({
-            sender: userid,
-            conversation,
-            message
-        })
-        const newMessage2 = newMessage.save()
-
-        return res.status(200).json({status: "OK"})
-    } catch(e) {
-        handleMongooseError(e as MongooseError, res)
-    }
-}
-
 // POST /api/message/first
 export const postFirstMessage = async (req: Request, res: Response) => {
     const {message, subject, broker} = req.body
@@ -106,3 +120,4 @@ export const postFirstMessage = async (req: Request, res: Response) => {
         handleMongooseError(e as MongooseError, res)
     }
 }
+*/
--- src/routes/messageRoutes.ts
+++ src/routes/messageRoutes.ts
@@ -1,15 +1,17 @@
 import { Router } from "express";
-import { getAllMessages, getLatestMessage, getMessages, postFirstMessage, postMessage } from "../controllers/messageController";
+import { getAllMessages, getMessage, getMessages, postMessage } from "../controllers/messageController";
 import isAuthenticated from "../middleware/isAuthenticated";
 
 const router = Router()
 
-// TEMP
-router.get("/all", getAllMessages)
-router.get("/latest/:id", isAuthenticated, getLatestMessage)
-router.get("/:id", isAuthenticated, getMessages)
+// Utility for dev, unsafe to run in prod
+//router.get("/all", getAllMessages)
+
+//router.get("/latest/:id", isAuthenticated, getLatestMessage)
+router.get("/:id", isAuthenticated, getMessage)
+router.get("/", isAuthenticated, getMessages)
 
 router.post("/", isAuthenticated, postMessage)
-router.post("/first", isAuthenticated, postFirstMessage)
+//router.post("/first", isAuthenticated, postFirstMessage)
 
 export default router
