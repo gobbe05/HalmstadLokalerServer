0e73a92af7fbb103b0d10aa67405ae18f7f7e776
Gabriel Raskov <gabriel@raskov.se>
2025-03-15 14:02:50 +0100

    Fixed issue. Might collide with issue 7

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
-import { deleteUser, getAllUsers, getAuth, getLogout, getMe, getUser, getUsername, postLogin, postRegister, putChangePassword} from "../controllers/authController";
+import { deleteUser, getAllUsers, getAuth, getLogout, getMe, getToAccept, getUser, getUsername, postLogin, postRegister, putAcceptUser, putChangePassword} from "../controllers/authController";
 import isAuthenticated from "../middleware/isAuthenticated";
+import adminProtection from "../middleware/adminProtection";
 
 const router = Router()
 
 router.get("/", isAuthenticated, getAuth)
 router.get("/me", isAuthenticated, getMe)
 router.get("/user", getAllUsers)
+router.get("/toaccept", isAuthenticated, adminProtection, getToAccept)
 router.get("/user/:username", getUser)
 router.get("/username/:id", getUsername)
 router.get("/logout", isAuthenticated, getLogout)
@@ -15,6 +17,7 @@ router.post("/login", postLogin);
 router.post("/register", postRegister)
 
 router.put("/changepassword", isAuthenticated, putChangePassword)
+router.put("/accept/:id", isAuthenticated, adminProtection, putAcceptUser)
 
 router.delete("/user/:id", deleteUser)
 
